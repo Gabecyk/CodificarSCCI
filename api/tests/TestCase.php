c878b2f2a20de82257500a1758ba732d13c01a43
Gabriel Balduino <gabrielbalduino29@gmail.com>
2026-09-24 15:09:43 -0300

    feat/fixing-testings-add-docker

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        $connection = $app['config']->get('database.default');
+
+        if ($connection !== 'sqlite') {
+            throw new RuntimeException(
+                "Testes abortados: a conexão \"{$connection}\" não é sqlite e seria apagada pelo RefreshDatabase."
+            );
+        }
+
+        return $app;
+    }
 }
